3aa57a24f260fc7b0d0c5760c6cc9cc5f2395cfa
Jakub Polanowski <jakubpol27@gmail.com>
2022-04-18 14:53:41 -0400

    expanded barrel

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,5 +1,9 @@
+export 'book_nav.dart';
 export 'bottom_nav.dart';
+export 'comments.dart';
 export 'error.dart';
 export 'loading.dart';
+export 'path_nav.dart';
+export 'permissions_editor.dart';
 export 'progress_bar.dart';
 export 'top_nav.dart';
