4341307583a8945cbb12459a5ae2b3f3c92070d4
Jakub Polanowski <jakubpol27@gmail.com>
2022-04-18 18:02:17 -0400

    added empty stateless widget

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+
 // TODO IMPLEMENT
+
+class LoginScrean extends StatelessWidget {
+  const LoginScrean({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
