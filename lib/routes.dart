493bd70d1895fab50b8107be46b92a15b2fb94e0
Jakub Polanowski <jakubpol27@gmail.com>
2022-04-18 18:30:44 -0400

    initial routes implementation

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,1 +1,23 @@
-// TODO IMPLEMENT
+import 'package:tome_stack/about/about.dart';
+import 'package:tome_stack/administration/administration.dart';
+import 'package:tome_stack/attachments/attachments.dart';
+import 'package:tome_stack/books/books.dart';
+import 'package:tome_stack/home/home.dart';
+import 'package:tome_stack/login/login.dart';
+import 'package:tome_stack/profile/profile.dart';
+import 'package:tome_stack/search/search.dart';
+import 'package:tome_stack/shelves/shelves.dart';
+import 'package:tome_stack/trash/trash.dart';
+
+var appRoutes = {
+  '/': (context) => const HomeScreen(),
+  '/login': (context) => const LoginScrean(),
+  '/about': (context) => const AboutScreen(),
+  '/shelves': (context) => const ShelvesScrean(),
+  '/books': (context) => const BooksScrean(),
+  '/attachments': (context) => const AttachementsScrean(),
+  '/search': (context) => const SearchScrean(),
+  '/trash': (context) => const TrashScrean(),
+  '/profile': (context) => const ProfileScrean(),
+  '/admin': (context) => const AdminScreen(),
+};
