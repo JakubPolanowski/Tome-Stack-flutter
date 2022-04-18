1ada2e127f08e348997f5f70eb5a550fac767e01
Jakub Polanowski <jakubpol27@gmail.com>
2022-04-18 13:12:57 -0400

    added emptry stateless widget and listed implementation goals

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+
 // TODO IMPLEMENT
+// Should have the following
+
+// 1) If logged in: My Recent Drafts & My Recently Viewed
+// 2) Recent Books
+// 3) Recently Updated Pages
+// 4) Recent Activity
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
